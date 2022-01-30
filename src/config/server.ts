a322bcb29682d908d5b0e95e99174d662664ee0e
Tjier Salazar <tjier.salazarvaros@gmail.com>
2022-01-29 23:20:22 -0300

    feat: add jest config

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,11 +3,11 @@ import { Logger } from '../handlers';
 
 export const start = (logger: Logger) => () => {
     try {
-        logger.info('[start server]: Init Server');
+        logger.info('[server -> start]: Init Server');
         const app = express();
         const port = process.env.PORT || 3000;
         app.listen(port, () => logger.info(`Server running in port: ${port}`));
     } catch (error) {
-        logger.error(`[start server]: Error in module. Error: ${error}`);
+        logger.error(`[server -> start]: Error: ${error}`);
     }
 };
